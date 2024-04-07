8b4d28cee9601ffcb059c20dfe10d0a37b6e33f9
babelpainterwell <zhang.zhongw@northeastern.edu>
2024-04-07 13:01:58 -0400

    update pe

--- model.py
+++ model.py
@@ -26,12 +26,16 @@ class PosionalEncoding(nn.Module):
         # PE(pos, 2i+1) = cos(pos/10000^{2i/d_model})
         encoding = torch.zeros(seq_len, d_model)
         position = torch.arange(0, seq_len, dtype=torch.float).unsqueeze(1)
-        div_term = 
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+
+        # div_term = torch.exp(torch.arange(0, d_model, 2).float()*(-math.log(10000.0) / d_model))
+        # div_term = 10000 ** (torch.arange(0, d_model,2) / d_model)
+        # encoding[:, 0::2] = torch.sin(position / div_term)
+        # encoding[:, 1::2] = torch.cos(position / div_term)
 
         encoding[:, 0::2] = torch.sin(position * div_term)
         encoding[:, 1::2] = torch.cos(position * div_term)
-        encoding = encoding.unsqueeze(0) # add a batch dimension in the front 
-
+        encoding = encoding.unsqueeze(0) # add a batch dimension in the front, [1, seq_len, d_model]
 
         # register a buffer to avoid the encoding from being considered as a learnable parameter 
         self.register_buffer("pe", encoding)
@@ -39,5 +43,7 @@ class PosionalEncoding(nn.Module):
     
     def forward(self, x):
         # add the positional encoding to the input sequence embedding [batch, seq_len, d_model]
-        x = x + self.pe[:, :x.shape[1], :]    # .requires_grad_(False) unnecessary if self.pe has been registered as a buffer
+        # .requires_grad_(False) unnecessary if self.pe has been registered as a buffer
+        # make sure we make the added positional encoding have the same length as the maximum input length
+        x = x + self.pe[:, :x.shape[1], :]
         return self.dropout(x)
